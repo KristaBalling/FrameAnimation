4816e140db4038874093bfbf2992d53c92ef8fc1
Krista Blachian <KristaBlachian@outlook.com>
2018-08-11 16:02:54 -0400

    app not working the way in should in the tutorial. Find update differences

--- app/src/main/java/com/theironyard/frameanimation/frameanimation/MainActivity.kt
+++ app/src/main/java/com/theironyard/frameanimation/frameanimation/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.theironyard.frameanimation.frameanimation
 
+import android.graphics.drawable.AnimationDrawable
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
 import android.widget.ImageView
 
-class MainActivity : AppCompatActivity() {
+ class MainActivity : AppCompatActivity() {
 
     private AnimationDrawable batAnimation;
     private ImageView batImage;
@@ -14,5 +16,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         batImage = (ImageView) findViewById(R.id.batID);
+        batImage.setBackgroundResource(R.drawable.bat_anim);
+        batAnimation = (AnimationDrawable) batImage.getBackground();
+
     }
-}
+
+     override fun onTouchEvent(event: MotionEvent?): Boolean {
+         batAnimation.start();
+         return super.onTouchEvent(event)
+     }
+ }
